7d0a3a406f07b87d7580624b15c2392d34e1c662
ravindrashrivastava <ravindra0491@gmail.com>
2023-10-12 17:09:03 +0530

    admin category title add remove function

--- admin/categories.php
+++ admin/categories.php
@@ -19,10 +19,27 @@
                         </h1>
                         <!--category form start -->
                         <div class="col-xs-6"> 
-                            <form action="">
+                            <?php 
+                            
+                            if (isset($_GET["submit"])){
+                                $cat_title = $_GET["cat_title"];
+
+                                if ($cat_title == "" || empty($cat_title)) {
+                                    echo "This field should not be empty";
+                                } else {
+                                    $query = "INSERT INTO category(cat_title) VALUE ('{$cat_title}')";
+                                    $create_category_query = mysqli_query($connection, $query);
+                                    if(!$create_category_query){
+                                        die("Query Failed".mysqli_error($connection));
+                                    }
+                                }
+                            }
+                            
+                            ?>
+                            <form action="categories.php" method="get">
                                 <div class="form-group">
                                     <label for="cat-title">Add Category</label>
-                                    <input type="text" class="form-control" name="cat-title">
+                                    <input type="text" class="form-control" name="cat_title">
                                 </div>
                                 <div class="form-group">
                                     <input type="submit" class="btn btn-primary" name="submit" value="Add Category">
@@ -31,31 +48,46 @@
                             </form>
                         </div> 
                         <!--category form end -->
-                        <table class="table table-bordered">
-                            <thead>
-                                <th>Id</th>
-                                <th>Category</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>asia</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>europe</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>amri</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>asia</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        
+                        <div class="col-xs-6">
+                            <?php 
+                                $query = "SELECT * FROM category";
+                                $select_category = mysqli_query($connection,$query);
+                               
+                            ?>
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <th>Id</th>
+                                    <th>Category</th>
+                                </thead>
+                                <tbody>
+
+                                    <?php 
+                                    while ($row = mysqli_fetch_assoc($select_category)) {
+                                        $cat_id = $row["cat_id"];
+                                        $cat_title = $row["cat_title"];
+                                        echo "<tr>";
+                                        echo "<td>{$cat_id}</td>" ;
+                                        echo "<td>{$cat_title}</td>" ;
+                                        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>" ;
+                                        echo "</tr>";
+                                    }
+                                
+                                    ?>
+
+                                    <?php 
+                                    
+                                    if (isset($_GET["delete"])) {
+                                    $del_cat_id = $_GET["delete"];
+                                    $query = "DELETE FROM category WHERE cat_id = {$del_cat_id}";
+                                    $delete_query = mysqli_query($connection, $query);
+                                    header("Location: categories.php");
+                                    }
+                                    
+                                    ?>
+                                    
+                                </tbody>
+                            </table>
+                        </div>        
                     </div>
                 </div>
                 <!-- /.row -->
@@ -63,5 +95,4 @@
             <!-- /.container-fluid -->
 
         </div>
-        <!-- /#page-wrapper -->
-<?php include "includes/admin_footer.php"; ?>
+        <!-- /#page-wrapper --> include "includes/admin_footer.php"; ?>
