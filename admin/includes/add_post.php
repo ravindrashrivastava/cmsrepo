cb9e46ba8859069818052373e09fa84a1636e045
ravindrashrivastava <ravindra0491@gmail.com>
2023-10-27 16:22:24 +0530

    add post php code added

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,3 +1,33 @@
+<?php 
+if (isset($_POST["create_post"])) {
+    $post_title = $_POST["title"];
+    $post_categoty_id = $_POST["post_categoty_id"];
+    $post_author = $_POST["post_author"];
+    $post_status = $_POST["post_status"];
+
+    $post_image = $_FILES["image"]["name"];
+    $post_image_temp =$_FILES["image"]["tmp_name"];
+
+    $post_tags = $_POST["post_tags"];
+    $post_content = $_POST["post_content"];
+    $post_date = date("d-m-y");
+    $post_title = $_POST["title"];
+    $post_comment_count = 4;
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+}
+
+
+
+
+?>
+
+
+
+
+
+
+
 <form action=""class="col-xs-6" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
@@ -5,7 +35,7 @@
     </div>
     <div class="form-group">
         <label for="title">Post Category ID</label>
-        <input type="text" class="form-control" name="title">
+        <input type="text" class="form-control" name="post_categoty_id">
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>
