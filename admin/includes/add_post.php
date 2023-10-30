1d5c184e46023a6dbdfc96088b63246f2090ec99
ravindrashrivastava <ravindra0491@gmail.com>
2023-10-30 15:32:38 +0530

    create post query

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,7 +1,8 @@
 <?php 
+
 if (isset($_POST["create_post"])) {
     $post_title = $_POST["title"];
-    $post_categoty_id = $_POST["post_categoty_id"];
+    $post_categoty_id = $_POST["post_category_id"];
     $post_author = $_POST["post_author"];
     $post_status = $_POST["post_status"];
 
@@ -15,6 +16,11 @@ if (isset($_POST["create_post"])) {
     $post_comment_count = 4;
 
     move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "INSERT INTO posts(post_category_id, post_title, post_author,post_date, post_image, post_content, post_tags, post_comment_count, post_status) VALUES ('{$post_categoty_id}', '{$post_title}', '{$post_author}',now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+    $connection = mysqli_connect("localhost","root","","cms");
+    $create_post_query = mysqli_query($connection, $query);
+
 }
 
 
@@ -35,7 +41,7 @@ if (isset($_POST["create_post"])) {
     </div>
     <div class="form-group">
         <label for="title">Post Category ID</label>
-        <input type="text" class="form-control" name="post_categoty_id">
+        <input type="text" class="form-control" name="post_category_id">
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>
