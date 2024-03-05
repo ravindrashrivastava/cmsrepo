402240cfb0ba1610b2dfbf25fb9977d4fcb7521d
ravindrashrivastava <ravindra0491@gmail.com>
2024-03-05 16:55:23 +0530

    chmod permission to htdocs

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -2,7 +2,7 @@
 
 if (isset($_POST["create_post"])) {
     $post_title = $_POST["post_title"];
-    $post_categoty_id = $_POST["post_category_id"];
+    $post_category_id = $_POST["post_category_id"];
     $post_author = $_POST["post_author"];
     $post_status = $_POST["post_status"];
 
@@ -15,9 +15,11 @@ if (isset($_POST["create_post"])) {
     $post_title = $_POST["post_title"];
     $post_comment_count = 0;
 
+
     move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author,post_date, post_image, post_content, post_tags, post_comment_count, post_status) VALUES ('{$post_categoty_id}', '{$post_title}', '{$post_author}',now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+    $query = "INSERT INTO posts (post_category_id, post_title, post_author,post_date, post_image, post_content, post_tags, post_comment_count, post_status) VALUES ('{$post_category_id}', '{$post_title}', '{$post_author}',now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}')";
+
 
     $connection = mysqli_connect("localhost","root","","cmsrepo");
 
@@ -39,8 +41,20 @@ if (isset($_POST["create_post"])) {
         <input type="text" class="form-control" name="post_title">
     </div>
     <div class="form-group">
-        <label for="title">Post Category ID</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <label for="title">PostCategory</label>
+        <select name="post_category_id" id="">
+            <option>Select Category</option>
+            <?php 
+            $que = "SELECT * FROM category";
+            $display_cat_que = mysqli_query($connection, $que);
+            while ($row = mysqli_fetch_assoc($display_cat_que)) {
+                $cat_title = $row["cat_title"];
+                $cat_id = $row["cat_id"];
+                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+            }
+            ?>
+
+        </select>
     </div>
     <div class="form-group">
         <label for="post_author">Post Author</label>
@@ -48,11 +62,14 @@ if (isset($_POST["create_post"])) {
     </div>
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <select name="post_status" id="">
+            <option value="draft">draft</option>
+            <option value="published">published</option>
+        </select>
     </div>
     <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input type="file" name="image">
+        <label for="">Post Image</label>
+        <input type="file" name="post_image">
     </div>
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
--- admin/index.php
+++ admin/index.php
@@ -32,6 +32,139 @@
                 </div>
                 <!-- /.row -->
 
+
+
+                       
+                <!-- /.row -->
+                
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-file-text fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                        
+                                            $que = "SELECT * FROM posts";
+                                            $post_count_que = mysqli_query($connection, $que);
+                                            $post_count = mysqli_num_rows($post_count_que);
+                                            echo "<div class='huge'>{$post_count}</div>"
+                                        
+                                        ?>
+                                        <div>Posts</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="posts.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                    <?php 
+                                        
+                                        $que = "SELECT * FROM comments";
+                                        $comment_count_que = mysqli_query($connection, $que);
+                                        $comment_count = mysqli_num_rows($comment_count_que);
+                                        echo "<div class='huge'>{$comment_count}</div>"
+                                    
+                                    ?>
+
+                                    <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="comments.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            
+                                            $que = "SELECT * FROM users";
+                                            $user_count_que = mysqli_query($connection, $que);
+                                            $user_count = mysqli_num_rows($user_count_que);
+                                            echo "<div class='huge'>{$user_count}</div>"
+                                        
+                                        ?>
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <?php 
+                                            
+                                            $que = "SELECT * FROM category";
+                                            $category_count_que = mysqli_query($connection, $que);
+                                            $category_count = mysqli_num_rows($category_count_que);
+                                            echo "<div class='huge'>{$category_count}</div>"
+                                        
+                                        ?>
+                                        <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="categories.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                                <!-- /.row -->
+
+
+
+
+
+
+
             </div>
             <!-- /.container-fluid -->
 
